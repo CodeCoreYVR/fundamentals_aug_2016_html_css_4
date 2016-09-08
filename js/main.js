e5c9e9561d1a3ad2ea702ba403eda6a3a9a5c8d7
Jacob Tran <jacobtran@gmail.com>
2016-09-07 21:01:23 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// single line comment
+
+
+/*
+
+  multi line comment
+  1
+  2
+  3
+  4
+  5
+
+*/
+
+
+// # this is a comment in ruby
+// a = 10
+var a = 10;
+var name = "Jacob";
+var array = [];
+var object = {};
+
+// array = Array.new
+var array = new Array();
+
+alert("Hello Fundamentals! This is JavaScript!");
+
+// document.getElementById('kaia').style = "background-color: lavender";
+
+window.onload = function() {
+  document.getElementById('kaia').style.backgroundColor = "lavender";
+  document.body.style.backgroundColor = "lavender";
+
+  //using javascript, change the company name in your jumbotron
+  document.getElementById("company-name").innerHTML = "Apple Computers Inc.";
+  //using javascript, change the brand name in your navbar
+  document.getElementById("nav-brand").innerHTML = "Apple Computers Inc.";
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
